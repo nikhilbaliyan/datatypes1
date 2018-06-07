8321472d7de9fdde256a62ee0d72dc6537293bf4
nikhilbaliyan <39897416+nikhilbaliyan@users.noreply.github.com>
2018-06-07 15:07:34 +0530

    Add files via upload

---
+++ question2.py
@@ -0,0 +1,9 @@
+wwe=[]
+name1=input("Enter the name of an star")
+wwe.append(name1)
+1name2=input("Enter the name of an star")
+wwe.append(name2)
+print(wwe)
+companies=['google','apple','facebook','microsoft','tesla']
+wwe.append(companies)
+print(wwe)
