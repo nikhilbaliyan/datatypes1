9a147eb13f6b1644d9ca756308233cff6c0bde8f
nikhilbaliyan <39897416+nikhilbaliyan@users.noreply.github.com>
2018-06-07 14:48:27 +0530

    Delete question3.py

--- question3.py
+++
@@ -1,4 +0,0 @@
-number1=int(input("enter value of x"))
-number2=int(input("enter value of y"))
-number3=int(input("enter value of z"))
-print("user has enterd the values of x,y and z",number1,number2,number3)
