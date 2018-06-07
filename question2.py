ea050a525df1b19869dcac4b16d48fac43d32ad0
nikhilbaliyan <39897416+nikhilbaliyan@users.noreply.github.com>
2018-06-07 15:07:13 +0530

    Delete question2.py

--- question2.py
+++
@@ -1,8 +0,0 @@
-my_list=['rock','romen','dean','seth','miz','john']
-my_list.append('google')
-my_list.append('google')
-my_list.append('apple')
-my_list.append('facebook')
-my_list.append('microsoft')
-my_list.append('tesla')
-print(my_list)
